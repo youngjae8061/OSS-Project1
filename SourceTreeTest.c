cd51d2eb127792f7edfdd42f9d1bcb5116b6917b
youngjaeLee8061 <young3966@gmail.com>
2020-05-20 10:07:17 +0900

    Test브랜치에 코드를 추가함

--- SourceTreeTest.c
+++ SourceTreeTest.c
@@ -1,8 +1,13 @@
 #include<stdio.h>
 
 int main(){
+  int i=0;
+
   printf("Fisrt OSS Code\n");
   printf("Second OSS Code\n");
 
+  for(int i=0; i<10; 1++){
+    print("Test 브랜치\n");
+  }
   return 0;
 }
